5807345604f226ec21fd90b6720bcc390decc971
Eman0001 <89368018+Eman0001@users.noreply.github.com>
2023-06-11 21:10:35 -0500

    module 5 part 2

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -9,7 +9,7 @@ const tripsList = async (req, res) => {
             if (!trips) {
                 return res
                     .status(404)
-                    .json({ "message": "trip not found" });
+                    .json({ "message": "trips not found" });
             } else if (err) {
                 return res
                     .status(404)
@@ -30,7 +30,7 @@ const tripsFindCode = async (req, res) => {
             if (!trip) {
                 return res
                     .status(404)
-                    .json({ "message": "trip not found" });
+                    .json({ "message": "trips not found" });
             } else if (err) {
                 return res
                     .status(404)
